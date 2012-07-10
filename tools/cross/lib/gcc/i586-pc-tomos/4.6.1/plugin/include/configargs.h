405795626bfd1d05bbb34842fcfc3a6f0a099c0e
Thomas Arcuri <tja@tjadev-linux.ANDROCS>
2012-07-10 08:38:57 -0400

    Added tools and libraries necessary for build.

---
+++ tools/cross/lib/gcc/i586-pc-tomos/4.6.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.6.1/configure --target=i586-pc-tomos --prefix=/home/tja/src/tomos/tools/cross --disable-nls --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
