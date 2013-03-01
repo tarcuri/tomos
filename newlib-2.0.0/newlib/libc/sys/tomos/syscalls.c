bc234b1057e9f3b9dec927cf610796cf4884ed3d
tarcuri <thomas.j.arcuri@gmail.com>
2013-03-01 13:58:08 -0500

    Freshly built versions of libc and libm from newlib-2.0.0.

---
+++ newlib-2.0.0/newlib/libc/sys/tomos/syscalls.c
@@ -0,0 +1,10 @@
+/* note these headers are all provided by newlib - you don't need to provide them */
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/times.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+#include <syscalls.h>   // from tomos
